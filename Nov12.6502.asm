00e565a731ff7f863472823d8d82f3e9282a8aab
audtheshaud <abookernelson@gmail.com>
2024-11-17 18:16:14 -0500

    Add File to Git Repo

---
+++ Nov12.6502.asm
@@ -0,0 +1,26 @@
+        .org            $0200
+
+        lda             #$aa
+        sta             $4000
+Delay1:
+        ldx             #$ff
+Delay2:
+        ldy             #$ff
+Delay3:
+        dey
+        bne Delay3
+        dex
+        bne Delay2
+        lda             #$55
+        sta             $4000
+Delay1b:
+        ldx             #$ff
+Delay2b:
+        ldy             #$ff
+Delay3b:
+        dey
+        bne Delay3b
+        dex
+        bne Delay2b
+
+        jmp             $0200
